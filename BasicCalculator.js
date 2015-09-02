715603338f500f5a7a33540bf9db6102e6d9bd37
David <dmwhite@gmail.com>
2015-09-01 23:05:54 -0400

    original commit of files

---
+++ BasicCalculator.js
@@ -0,0 +1,12 @@
+console.assert(add(0, 0) === 0);
+console.assert(add(0, 1) === 1);
+console.assert(add(0, 2) === 2);
+console.assert(add(0, 3) === 3);
+/**
+ * @param {Number} A to add to B
+ * @param {Number} B to add to A
+ * @return {Number} sum of A and B
+ */
+function add(A, B){
+return A+B;
+} // END add
