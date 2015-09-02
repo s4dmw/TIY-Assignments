db4c131888ed2e552d6ef1a9a68554aa22b9a982
David <dmwhite@gmail.com>
2015-09-01 23:09:31 -0400

    added 0+4 up to 0+6

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -2,6 +2,14 @@ console.assert(add(0, 0) === 0);
 console.assert(add(0, 1) === 1);
 console.assert(add(0, 2) === 2);
 console.assert(add(0, 3) === 3);
+console.assert(add(0, 4) === 4);
+console.assert(add(0, 5) === 5);
+console.assert(add(0, 6) === 6);
+
+
+
+
+
 /**
  * @param {Number} A to add to B
  * @param {Number} B to add to A
