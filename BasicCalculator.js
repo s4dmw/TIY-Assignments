70d6720a6ea87c4858e4498f9e90beb62b2fd5b1
David <dmwhite@gmail.com>
2015-09-02 17:19:11 -0400

    updated minu function test cases

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -26,7 +26,16 @@ return A+B;
 
 
 console.assert(minus(0, 0) === 0);
-console.assert(minus(1, 0) === 1);
+console.assert(minus(0, 1) === -1);
+console.assert(minus(0, 2) === -2);
+console.assert(minus(0, 3) === -3);
+console.assert(minus(0, 4) === -4);
+console.assert(minus(0, 5) === -5);
+console.assert(minus(0, 6) === -6);
+console.assert(minus(0, 7) === -7);
+console.assert(minus(0, 8) === -8);
+console.assert(minus(0, 9) === -9);
+
 
 function minus(A, B){
   return A-B;
