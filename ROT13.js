ee7e5c457f683616d00ca8f09c06c03c9fdbb139
David <dmwhite@gmail.com>
2015-09-04 11:14:15 -0400

    starting point for reverse function

---
+++ ROT13.js
@@ -0,0 +1,24 @@
+/**
+ * The function `reverse` computes the reversal
+ * of a given `String` (don't copy-pasta!)
+ *
+ * @param {String} S to reverse
+ * @return {String}
+ */
+function reverse(S){
+    // YOUR CODE HERE: NO INTERNET COPY-PASTA!
+}
+
+console.assert(reverse("") === ""); // Really?
+
+console.assert(reverse("A") === "A"); // Jerk.
+
+console.assert(reverse("cat") === "tac");
+
+console.assert(reverse("ward") === "draw");
+
+console.assert(reverse("books") === "skoob"); // Rokey, dokey...
+
+console.assert(
+  reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew"
+);
