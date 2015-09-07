91e69f19327f80376bdbc618a6a801fb9a4fdbd5
David <dmwhite@gmail.com>
2015-09-06 20:29:03 -0400

    got the sum working for evens

--- EvenFibonaccis.js
+++ EvenFibonaccis.js
@@ -1,5 +1,6 @@
 var test = require('mocha').it,
   expect = require('chai').expect;
+  assert = require('chai').assert;
 
 test('generating the Fibonacci sequence', function(){
   expect(fibonacci(-1)).to.deep.equal([ ]);
@@ -12,6 +13,9 @@ test('generating the Fibonacci sequence', function(){
   expect(fibonacci(6)).to.deep.equal([1, 2, 3, 5, 8, 13]);
   expect(fibonacci(7)).to.deep.equal([1, 2, 3, 5, 8, 13, 21]);
   expect(fibonacci(8)).to.deep.equal([1, 2, 3, 5, 8, 13, 21, 34]);
+  expect(fibonacci(10)).to.deep.equal([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]);
+
+
 
   // ...ad nauseum.
 }); // END test(fibonacci)
@@ -55,18 +59,48 @@ function evens(N) {  //returns the even numbers of a given array N
 
 
 
-/*
+
 test('summing lists of Number', function(){
   assert.equal(sum([ ]), 0);
   assert.equal(sum([ 1 ]), 1);
   assert.equal(sum([ 1, 1 ]), 2);
-  // ...et cetera, et cetera, et cetera.
+  assert.equal(sum([2, 8]), 10);
+  assert.equal(sum([2, 8, 34]), 44);
+  assert.equal(sum(evens(fibonacci(0))), 0);
+  assert.equal(sum(evens(fibonacci(1))), 0);
+  assert.equal(sum(evens(fibonacci(2))), 2);
+  assert.equal(sum(evens(fibonacci(3))), 2);
+  assert.equal(sum(evens(fibonacci(8))), 44);
+
+
 }); // END test(sum)
 
-// You expect me to write `sum`, too?
+function sum (N) {
+  var sum = 0
+  for (count = 0; count < N.length; count ++) {
+    sum = sum + N[count];
+  }
+  return sum;
+
+}
+
+// oops, kinda already did the testing below...but i'll do it again just for "fun"
 
 test('summing even Fibonacci numbers?', function(){
-  expect( sum(evens(fibonacci(0))) ).to.equal(FILL_ME_IN); // nice.
-  // There really should be more code in here...
+  expect( sum(evens(fibonacci(0))) ).to.equal(0);
+  expect(sum(evens(fibonacci(1)))).to.equal(0);
+  expect(sum(evens(fibonacci(2)))).to.equal(2);
+  expect(sum(evens(fibonacci(3)))).to.equal(2);
+  expect(sum(evens(fibonacci(10)))).to.equal(44);
+  expect(sum(evens(fibonacci(20)))).to.equal(14328); //getting hard to do in my head
+  expect(sum(evens(fibonacci(50)))).to.equal(26658145586);
+  expect(sum(evens(fibonacci(100)))).to.equal(1.5005088278427221e+21); // lol
+  expect(sum(evens(fibonacci(1000)))).to.equal(1.8412729310978296e+209); // wtf is this #?
+  expect(sum(evens(fibonacci(10000)))).to.equal(Infinity); //uh oh
+
+
+
+
+
+
 }); // END test(dat CHAIN tho)
-*/
