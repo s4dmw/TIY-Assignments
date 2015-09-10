69102068f13af4be29b7abc2b5daef678373b5b8
David <dmwhite@gmail.com>
2015-09-09 21:49:05 -0400

    added some english to the scripts file

---
+++ NestedAccordion/js/scripts.js
@@ -0,0 +1,13 @@
+// What does it do? - headers/sections accoridion down when clicked on
+// What interactive elements exist? - the arrows
+// How do you interact with them? - hovering over them or clicking on them
+// What visual effects are produced by interacting? - arrows change
+// How does it do it? cbt-ntopen? cbp-nttrigger?
+// What existing HTML elements are changed?
+// What new HTML elements are created? li classes are changed
+// What CSS styles are used to produce the effect? cbt-ntopen? cbp-nttrigger?
+//
+// need to somehow accoridion the h3's and h4's
+// maybe cbp-nttrigger and cbp-ntopen?
+//
+// also need to hightlight the icons in the top right corner when hovered over
