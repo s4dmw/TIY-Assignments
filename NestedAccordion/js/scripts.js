68b7b53aef6a4f508f8b4433a045a2b68d8cdb14
David <dmwhite@gmail.com>
2015-09-09 21:56:29 -0400

    added DOM elements i think we might need

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -2,12 +2,12 @@
 // What interactive elements exist? - the arrows
 // How do you interact with them? - hovering over them or clicking on them
 // What visual effects are produced by interacting? - arrows change
-// How does it do it? cbt-ntopen? cbp-nttrigger?
+// How does it do it? addEventListener and dispatchEvent?
 // What existing HTML elements are changed?
 // What new HTML elements are created? li classes are changed
 // What CSS styles are used to produce the effect? cbt-ntopen? cbp-nttrigger?
 //
 // need to somehow accoridion the h3's and h4's
-// maybe cbp-nttrigger and cbp-ntopen?
+// maybe addEventListener and dispatchEvent?
 //
 // also need to hightlight the icons in the top right corner when hovered over
