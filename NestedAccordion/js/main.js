759e7578cee642175c765c027138d9c94fde0fdf
David <dmwhite@gmail.com>
2015-09-26 13:56:59 -0400

    got the accordions/subacordions working

--- NestedAccordion/js/main.js
+++ NestedAccordion/js/main.js
@@ -1,13 +1,18 @@
 // console.log("tracer bullet") //making sure index.html is running main.js
 
 
-// function to open/close h3 elements
-// $("h3").on("click", function(){
-//   console.log("h3 tracer bullet");
-//   $("li").removeClass("closed");
-// });
-
+// open/close accordions with h3 headers
 $("h3").click(function(){
-  console.log("h3 click");
+  // console.log("h3 click");
+  $(this).toggleClass("closed");
+});
+
+// open/close accordions with h4 headers
+$("h4").click(function(){
+  $(this).toggleClass("closed");
+});
+
+// open/close accordions with h2 headers
+$("h2").click(function(){
   $(this).toggleClass("closed");
 });
