18e99e26f93c61b8148a4d2645946a13fca2f98c
David <dmwhite@gmail.com>
2015-09-27 17:44:26 -0400

    added arrows to nested accordion

--- NestedAccordion/js/main.js
+++ NestedAccordion/js/main.js
@@ -4,15 +4,15 @@
 // open/close accordions with h3 headers
 $("h3").click(function(){
   // console.log("h3 click");
-  $(this).toggleClass("closed");
+  $(this).toggleClass("closed open");
 });
 
 // open/close accordions with h4 headers
 $("h4").click(function(){
-  $(this).toggleClass("closed");
+  $(this).toggleClass("closed open");
 });
 
 // open/close accordions with h2 headers
 $("h2").click(function(){
-  $(this).toggleClass("closed");
+  $(this).toggleClass("closed open");
 });
